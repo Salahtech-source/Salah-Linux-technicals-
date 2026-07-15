a9e6337a65908ca6810da20067b08471d74b48a5
salah hildid <salahhildid2006@gmail.com>
2026-07-15 14:50:18 +0100

    comitted two files

--- abdul_bash.sh
+++ abdul_bash.sh
@@ -10,3 +10,7 @@ ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 11
 echo ""
 echo "===== Top 10 Processes by Memory Usage ====="
 ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%mem | head -n 11
+
+
+
+
