c24c7017ddb437744b208dc560501c8e75b904d8
Salah <salahhildid2006@gmail.com>
2026-07-15 11:11:44 +0100

    Adding new bash script

---
+++ abdul_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "===== Memory Usage ====="
+free -h
+
+echo ""
+echo "===== Top 10 Processes by CPU Usage ====="
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 11
+
+echo ""
+echo "===== Top 10 Processes by Memory Usage ====="
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%mem | head -n 11
